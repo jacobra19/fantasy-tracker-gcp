a947bf5eb1f0dce29a6903b74f2e75fa9e094363
Yakov Rakhamimov <jacobra19@gmail.com>
2021-01-08 23:04:46 +0200

    updated function for daily scrape

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,13 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin')
 const { scrape } = require('./scrapeRosters/executePuppeteer')
+const { rookies2020 } = require('./scrapeRosters/rookies')
 
-// const now = admin.firestore.Timestamp.now()
-const cronFormat = '30 * * * *' // run every night at 04:30
+admin.initializeApp()
+
+
+const now = admin.firestore.Timestamp.now()
+const cronFormat = '30 4 * * *' // run every night at 04:30
 
 
 const runtimeOpts = {
@@ -10,14 +15,23 @@ const runtimeOpts = {
     memory: '2GB',
 }
 
-exports.updateDailyRosters = functions.runWith(runtimeOpts).pubsub.schedule(cronFormat).timeZone('Asia/Jerusalem').onRun(async (context)=> {
-    console.log('before')
+exports.updateDailyRosters = functions.runWith(runtimeOpts).pubsub.schedule(cronFormat).timeZone('Asia/Jerusalem').onRun(async (context) => {
     functions.logger.info("before", { structuredData: true });
 
-    let dailyRoster = await scrape()
-    console.log('dailyRoster', dailyRoster)
+    let dailyRoster = await scrape(rookies2020)
 
-    console.log('after')
+    let objectToSave = {
+        isRookieStatusValid: dailyRoster.every(item => {
+            return item.rooks.length > 0
+        }),
+        rosters: dailyRoster,
+        time: now.toDate()
+    }
+
+    let weriteResult = await admin.firestore().collection('daily-rosters').add(objectToSave)
+
+    functions.logger.info(weriteResult, { structuredData: true });
+
     functions.logger.info("after", { structuredData: true });
     return null;
 })
